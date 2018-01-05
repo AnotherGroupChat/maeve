b56f26780a1046a888edeb56512bf8154e4c06dc
dylan <contact@postmodern.technology>
2018-01-05 00:15:13 -0800

    Established proto building and basic infrastructure for future work.

---
+++ build.sh
@@ -0,0 +1,8 @@
+#Clear out old generated files
+rm src/protos/*.*
+
+# Builds the game from a text file into the needed binary format.
+cat games/sample.pbtxt | protoc --encode=Maeve.Game protos/*.proto > games/sample.pb
+
+# Generate new files
+protoc --rust_out src/protos protos/master.proto
