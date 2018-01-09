d73933b7e93a7eed462a3c97b10f14e7e0bd3829
dylan <contact@postmodern.technology>
2018-01-09 01:25:12 -0800

    Added args and file reading

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
 #Clear out old generated files
-rm src/protos/*.*
+rm src/protos/master.rs
 
 # Builds the game from a text file into the needed binary format.
 cat games/sample.pbtxt | protoc --encode=Maeve.Game protos/*.proto > games/sample.pb
 
 # Generate new files
-protoc --rust_out src/protos protos/master.proto
+protoc --rust_out src/protos protos/*.proto
