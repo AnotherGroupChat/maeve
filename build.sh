87e986dcd1bc9e20f6c0b679104c64379b1bb5e6
Andrew Zah <zah@andrewzah.com>
2018-01-23 15:19:28 -0500

    Make build.sh modular; accept custom proto names

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
+# either use 'game_design' as the default or
+# a user supplied name
+filename=${1:-"game_design"}
+
 #Clear out old generated files
 if [ -z "src/protos/master.rs" ]; then
   rm src/protos/master.rs
 fi
 
 # Builds the game from a text file into the needed binary format.
-cat games/game_design.pbtxt | protoc --encode=Maeve.Game protos/*.proto > games/game_design.pb
+cat games/$filename.pbtxt | protoc --encode=Maeve.Game protos/*.proto > games/$filename.pb
 
 # Generate new files
 protoc --rust_out src/protos protos/*.proto
