bdd17000988c00471f453909f0f9479b2df409a6
kevinlmadison <coolklm121@gmail.com>
2018-02-03 00:49:29 -0500

    formatted for travis

--- src/io.rs
+++ src/io.rs
@@ -1,13 +1,13 @@
 //! Operations that interact with the file system.
 
+use load::save;
 use std::fs::File;
 use std::path::Path;
 use protobuf::core::MessageStatic;
 use protobuf::CodedOutputStream;
 use protobuf::Message; 
-use screen::Interfaceable;
 use protos::master::Game;
-use load::save;
+use screen::Interfaceable;
 
 pub fn extract_protobuf<F, M: MessageStatic, I: Interfaceable>(
     src: &mut I,
@@ -18,12 +18,10 @@ where
     F: Fn(&mut I, M) -> Result<M, String>,
 {
     return match File::open(&Path::new(path)) {
-        Ok(mut is) => {
-            match ::protobuf::parse_from_reader::<M>(&mut is) {
+        Ok(mut is) => match ::protobuf::parse_from_reader::<M>(&mut is) {
                 Ok(t) => callback(src, t),
                 Err(_) => Err(String::from("Failed to load file.")),
-            }
-        }
+        },
         Err(_) => Err(String::from("Failed to open file")),
     };
 }
@@ -32,7 +30,6 @@ pub fn write_protobuf<I: Interfaceable>(
     src: &mut I,
     mut game: Game,
 ) -> Result<Game, String> {
-
     let mut file = save(src, &game);
     let mut cos = CodedOutputStream::new(&mut file);
     game.write_to(&mut cos);
